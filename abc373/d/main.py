69bc74014f3d1671e0bff667462f9e3bd37cd7e2
takechi-scratch <takechi.scratch@gmail.com>
2024-11-02 20:58:52 +0900

    add abc373d WA

--- abc373/d/main.py
+++ abc373/d/main.py
@@ -1,2 +1,31 @@
-# N = int(input())
-# A = [int(x) for x in input().split()]
+# 後日解答、解説AC。
+from collections import deque
+
+N, M = [int(x) for x in input().split()]
+ans = [None] * N
+cost = [{} for _ in range(N)]
+
+for _ in range(M):
+    u, v, w = [int(x) for x in input().split()]
+    cost[u - 1][v - 1] = w
+
+for i in range(N):
+    if ans[i] is not None or len(cost[i]) == 0:
+        continue
+
+    ans[i] = 0
+
+    bfs = deque([])
+    bfs.extend([(i, x) for x in cost[i].keys()])
+
+    while len(bfs) > 0:
+        u, v = bfs.popleft()
+        w = cost[u][v]
+
+        if ans[v] is None:
+
+            ans[v] = ans[u] + w
+
+            bfs.extend([(u, x) for x in cost[u].keys()])
+
+print(" ".join([str(x) for x in ans]))
