313312998332c91a5e94978a7ee459ede50682a2
takechi-scratch <takechi.scratch@gmail.com>
2025-02-09 11:24:20 +0900

    add abc392 writing

--- abc392/d/main.py
+++ abc392/d/main.py
@@ -8,6 +8,7 @@ for _ in range(N):
     input_die = [int(x) for x in input().split()]
     dice_mens.append(input_die[0])
     for i in range(1, len(input_die)):
+        # 各数字が何面あるかを記録
         die[input_die[i]] += 1
 
     dice.append(die)
@@ -17,10 +18,12 @@ for p in range(N):
     for q in range(p):
         d1, d2 = dice[p], dice[q]
         # WAポイント！和集合と共通部分を正しく選ぶ。
+        # 和集合にしたらTLEになりました
         search_number = set(d1.keys()) & set(d2.keys())
 
         next_ans = 0
         for i in search_number:
+            # どちらもその数字が出る確率を足していく
             next_ans += d1[i] * d2[i] / dice_mens[q] / dice_mens[p]
 
         ans = max(ans, next_ans)
--- abc392/e/main.py
+++ abc392/e/main.py
@@ -1,4 +1,5 @@
 # https://github.com/not522/ac-library-python/blob/master/atcoder/dsu.py
+# UnionFind初AC！
 
 import typing
 
@@ -81,14 +82,17 @@ for i in range(M):
     sides[a].append(b)
     sides[b].append(a)
     if unionfind.same(a, b):
+        # 余りの辺があったら、番号と伸ばし先を取っておく
         shareable[a].append(b)
         shareable_num[a].append(i + 1)
     else:
         unionfind.merge(a, b)
 
 groups = unionfind.groups()
+# グループを余りのケーブルで1本ずつつないでいけば、グループ数-1の回数でOK
 print(len(groups) - 1)
 
+# グループごとに余りの辺をまとめる
 group_shareable = [[] for _ in range(len(groups))]
 group_shareable_num = [[] for _ in range(len(groups))]
 for i, group in enumerate(groups):
@@ -96,6 +100,7 @@ for i, group in enumerate(groups):
         group_shareable[i].extend(shareable[j])
         group_shareable_num[i].extend(shareable_num[j])
 
+# 「今つながっている最小のグループ番号」
 min_ok = 0
 for i in range(len(groups)):
     if min_ok >= len(groups) - 1:
@@ -105,12 +110,19 @@ for i in range(len(groups)):
         ans_from = group_shareable[i][j]
         ans_num = group_shareable_num[i][j]
         if min_ok + 1 == i:
+            # つなげなければいけないグループが今の余りと同じとき
+            # グループ0につなげておけばOK
             print(ans_num, ans_from + 1, groups[0][0] + 1)
-            # unionfind.merge(ans_from, groups[0][0]) # あとけす
+            unionfind.merge(ans_from, groups[0][0])  # 確認用（提出時は消す）
         else:
+            # 異なるときは、つなげるグループ番号の中のサーバー1つにつなげる
             print(ans_num, ans_from + 1, groups[min_ok + 1][0] + 1)
-            # unionfind.merge(ans_from, groups[min_ok + 1][0]) # あとけす
+            unionfind.merge(ans_from, groups[min_ok + 1][0])  # 確認用（提出時は消す）
         min_ok += 1
 
+        # 全てつながったときは終了
         if min_ok >= len(groups) - 1:
+            assert len(unionfind.groups()) == 1  # 確認用（提出時は消す）
             exit()
+
+assert len(unionfind.groups()) == 1  # 確認用（提出時は消す）
