a491531b5552ed1b22bb06e1050a8557c3c6cae8
takechi-scratch <takechi.scratch@gmail.com>
2025-02-13 18:21:25 +0900

    add abc392f

--- abc392/f/main.py
+++ abc392/f/main.py
@@ -1,2 +1,19 @@
-# N = int(input())
-# A = [int(x) for x in input().split()]
+# 解説AC。
+# 「逆順」を考えるのは思いついていた→位置をリストに取っておいて、
+# そこから逆算するとそれぞれの位置が分かるようになる！！
+
+from sortedcontainers import SortedSet
+
+N = int(input())
+P = [int(x) - 1 for x in input().split()]
+ans = [-1] * N
+
+last_pos = SortedSet(range(N))
+
+for i in range(N - 1, -1, -1):
+    pos = last_pos.pop(P[i])
+    ans[pos] = i + 1
+
+print(*ans)
+
+# びっくりするほどシンプルな解答になった
