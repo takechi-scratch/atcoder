03beedbf2f49ddffba639b864cad9fe059b26559
takechi-scratch <takechi.scratch@gmail.com>
2025-02-22 14:44:59 +0900

    add joi2025yo1a_d

--- abc393/e/main.py
+++ abc393/e/main.py
@@ -1,17 +1,23 @@
-# setだとメモ化再帰はできない！なんか使ってみようと思ったけどダメ。
-
-from functools import lru_cache
-from math import gcd
-
 N, K = [int(x) for x in input().split()]
 A = [int(x) for x in input().split()]
 
-@lru_cache(maxsize=10 ** 9)
-def many_gcd(numbers: set):
-    a = numbers.pop()
-    if len(numbers) <= 0:
-        return a
+max_A = max(A)
+count = [0] * max_A
+for x in A:
+    count[x - 1] += 1
 
-    return gcd(many_gcd(numbers), a)
+multiple_count = [0] * max_A
+for i in range(1, max_A + 1):
+    for j in range(i, max_A + 1, i):
+        multiple_count[i - 1] += count[j - 1]
 
-assert many_gcd(set([3,6,9])) == 3
+ans = [0] * max_A
+for i in range(1, max_A + 1):
+    if multiple_count[i - 1] < K:
+        continue
+
+    for ans_to in range(i, max_A + 1, i):
+        ans[ans_to - 1] = i
+
+for x in A:
+    print(ans[x - 1])
