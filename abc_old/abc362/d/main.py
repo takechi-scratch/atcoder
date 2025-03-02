bd81bf3e68ae1f64e2b903335118f20933fa82f4
takechi-scratch <takechi.scratch@gmail.com>
2025-03-02 11:55:49 +0900

    add abc362d

--- abc_old/abc362/d/main.py
+++ abc_old/abc362/d/main.py
@@ -1,42 +1,38 @@
-# TLE解法。ダイクストラ法を学んでからまた来ます。
+# ダイクストラ法の練習
+# ABC395Eを簡単めにした感じ（そっちにも応用できる）
 
-from collections import deque
+import heapq
 
 N, M = [int(x) for x in input().split()]
 A = [int(x) for x in input().split()]
-sides = {}
-costs = {}
+sides = [[] for _ in range(N)]
+costs = [[] for _ in range(N)]
 for _ in range(M):
     u, v, b = [int(x) for x in input().split()]
-    if u - 1 not in sides:
-        sides[u - 1] = []
-        costs[u - 1] = []
     sides[u - 1].append(v - 1)
     costs[u - 1].append(b)
 
-    if v - 1 not in sides:
-        sides[v - 1] = []
-        costs[v - 1] = []
     sides[v - 1].append(u - 1)
     costs[v - 1].append(b)
 
-all_ans = []
-for goal in range(1, N):
-    ans = 10 ** 18
-    bfs = deque([(0, {0}, A[0])])
-    while len(bfs) > 0:
-        now, been, cost = bfs.popleft()
-        for i in range(len(sides[now])):
-            to = sides[now][i]
-            if to in been:
-                continue
+min_cost = [10 ** 18] * N
+# min_cost[0] = A[0]  # こういう先初期化はWAのもとかも
+queue = [(A[0], 0)]
+heapq.heapify(queue)
 
-            if to == goal:
-                ans = min(ans, cost + costs[now][i] + A[to])
-                continue
+while len(queue) > 0:
+    now_cost, now = heapq.heappop(queue)
+    # 継続をはじくコードを置いておかないと、TLE（無限ループ？）になった
 
-            bfs.append((to, been | {to}, cost + costs[now][i] + A[to]))
+    if min_cost[now] <= now_cost:
+        continue
 
-    all_ans.append(ans)
+    min_cost[now] = min(min_cost[now], now_cost)
 
-print(*all_ans)
+    for next_node, next_side_cost in zip(sides[now], costs[now]):
+        next_cost = now_cost + next_side_cost + A[next_node]
+
+        # 上に書いたので、ここには書かなくてOK
+        heapq.heappush(queue, (next_cost, next_node))
+
+print(*min_cost[1:])
