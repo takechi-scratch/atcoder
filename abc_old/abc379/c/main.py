9cc719ea550981ced6cf4d51a3e6dbfe716317b6
takechi-scratch <takechi.scratch@gmail.com>
2025-03-19 19:57:46 +0900

    fix abc379c

--- abc_old/abc379/c/main.py
+++ abc_old/abc379/c/main.py
@@ -1,40 +1,24 @@
-# WA解答。後ろから見ていっているのが良くない？
-
 N, M = [int(x) for x in input().split()]
 X = [int(x) - 1 for x in input().split()]
 A = [int(x) for x in input().split()]
 
-def solve():
-    # 合計があっていない場合はアウト。ここはできてた！
-    if sum(A) != N:
+# 合計があっていない場合はアウト。ここはできてた！
+if sum(A) != N:
+    print(-1)
+    exit()
+
+# WAポイント！入力がソートされているかしっかり確認する。
+# かなりの参加者が見落としがちだったらしい。
+li = (sorted(list(zip(X, A)), key=lambda x: x[0]))
+
+sum_stones = 0
+for x in reversed(li):
+    sum_stones += x[1]
+    if sum_stones > N - x[0]:
         print(-1)
-        return
-
-    # WAポイント！入力がソートされているかしっかり確認する。
-    # かなりの参加者が見落としがちだったらしい。
-    li = (sorted(list(zip(X, A)), key=lambda x: x[0]))
-    # print(li)
-
-    ans = 0
-    w_boxes = 0
-    bef_i = N
-
-    # 後ろから見るときのforループは-1を連打。テンプレコード
-    for i in range(M-1, -1, -1):
-        w_boxes += bef_i - li[i][0] - 1
-
-        if w_boxes < li[i][1] - 1:
-            print(-1)
-            return
-
-        # 元から石が置かれてる場所をまたぐときの処理も考えないといけない。
-        ans += (w_boxes + 1) * w_boxes // 2
-
-        w_boxes -= li[i][1] - 1
-        bef_i = li[i][0]
+        exit()
 
 
-    print(ans)
-
-
-solve()
+# 合計はこれで簡単に計算できる！
+now_score = sum(x * y for x, y in li)
+print(N * (N - 1) // 2 - now_score)
