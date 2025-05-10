f2471f2f94499138c7a2277bc8d244328fabef8f
takechi-scratch <takechi.scratch@gmail.com>
2025-05-10 23:08:04 +0900

    debug abc405e

--- abc405/e/main.py
+++ abc405/e/main.py
@@ -7,19 +7,20 @@ from functools import lru_cache
 MOD = 998244353
 
 A, B, C, D = [int(x) for x in input().split()]
-N = A + B + C + D
 
 factor = [1]
 for i in range(1, A + B + C + D):
     factor.append(factor[-1] * i % MOD)
+    inv_factor.append(pow(factor[-1], -1, MOD))
+inv_factor = [None] * len(factor)
 
-def mod_factorical(n: int, MOD: int = 998244353):
+def mod_factorical(n: int):
     return factor[n]
 
 @lru_cache(maxsize=None)
 def mod_comb(n: int, k: int, MOD: int = 998244353):
-    ans = mod_factorical(n, MOD) * pow(mod_factorical(k, MOD), -1, MOD) % MOD
-    ans = ans * pow(mod_factorical(n - k, MOD), -1, MOD) % MOD
+    ans = mod_factorical(n) * inv_factor[k] % MOD
+    ans = ans * inv_factor[n - k] % MOD
     return ans
 
 ans = 0
@@ -30,7 +31,11 @@ for A_end in range(A, A + B + 1):
     first_area = mod_comb(A_end - 1, first_B)
 
     second_third = mod_factorical(second_B + C + D)
-    second_third *= pow(mod_factorical(second_B) * mod_factorical(C) % MOD * mod_factorical(D) % MOD * mod_comb(second_B + D, D) % MOD, -1, MOD)
+    second_third = second_third * inv_factor[second_B] % MOD
+    second_third = second_third * inv_factor[C] % MOD
+    second_third = second_third * inv_factor[D] % MOD
+
+    second_third *= pow(mod_comb(second_B + D, D), -1, MOD)
 
     ans += first_area * second_third
     ans %= MOD
