bc68b589630e5004c5377d3b45a7a27569ceb047
takechi-scratch <takechi.scratch@gmail.com>
2025-06-07 09:24:19 +0900

    update ahc_script

--- ahc/test.sh
+++ ahc/test.sh
@@ -1,4 +1,14 @@
+rm answer/*
+rm message.txt
+
+trap 'echo "\n中断されました"; exit 130' INT
+
 for f in tests/*.in; do
   filename=$(basename "$f" .in)
-  python main.py < "$f" > "answer/${filename}.out"
+  # 標準エラー出力をリアルタイムでコンソールに出しつつ、message.txtにも追記
+  python main.py < "$f" > "answer/${filename}.out" 2> >(tee -a message.txt >&2)
 done
+
+trap - INT
+
+grep 'Score:' message.txt | sed -E 's/.*Score: *([0-9]+).*/\1/' | awk '{sum+=$1} END{print sum}'
