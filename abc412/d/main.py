e6e8da833f257206e79a75a7d0e0e7e3c7595227
takechi-scratch <takechi.scratch@gmail.com>
2025-07-05 20:56:46 +0900

    add abc_upsolve

--- abc412/d/main.py
+++ abc412/d/main.py
@@ -1,25 +1,63 @@
+def divide_group(N: int):
+    """0からN未満までの整数をグループ分けするとき、考えられる組み合わせを列挙
+       N<=10くらいまでなら問題なし
+
+    Args:
+        N (int): 要素の数
+
+    """
+
+    assert N >= 0
+
+    dfs = [(0, [])]
+    while len(dfs) > 0:
+        finished_count, groups = dfs.pop()
+        if finished_count >= N:
+            yield groups
+            continue
+
+        for i in range(len(groups) + 1):
+            next_group = [set(y for y in x) for x in groups]
+            if i == len(next_group):
+                next_group.append({finished_count})
+            else:
+                next_group[i].add(finished_count)
+
+            dfs.append((finished_count + 1, next_group))
+
+# -------------------------------------------------------------
+
 import itertools
-import more_itertools
 
 N, M = [int(x) for x in input().split()]
-sides = [[] for _ in range(N)]
+sides = [set() for _ in range(N)]
 for _ in range(M):
     a, b = [int(x) - 1 for x in input().split()]
-    sides[a].append(b)
-    sides[b].append(a)
+    sides[a].add(b)
+    sides[b].add(a)
 
-def divide_group(N: int):
-    d = list(range(N))
-    l = list(more_itertools.powerset(d))[1:]
+ans = 10 ** 18
+for groups in divide_group(N):
+    if not all(len(x) >= 3 for x in groups):
+        continue
 
-    for i in range(1, N + 1):
-        for ans in itertools.combinations(l, i):
-            check = []
-            for x in ans:
-                check.extend(x)
+    now_divide_ans = 0
+    need_sides = [set() for _ in range(N)]
+    for group in groups:
+        now_group_ans = 10 ** 18
+        for perm in itertools.permutations(group):
+            for i in range(-1, len(group) - 1):
+                need_sides[perm[i]].add(perm[i + 1])
+                need_sides[perm[i + 1]].add(perm[i])
 
-            if len(check) == N and sorted(check) == d:
-                yield ans
+            now_ans = 0
+            for i in group:
+                now_ans += len(sides[i] ^ need_sides[i])
 
-for x in divide_group(N):
-    print(x)
+            now_group_ans = min(now_group_ans, now_ans)
+
+        now_divide_ans += now_group_ans
+
+    ans = min(ans, now_divide_ans // 2)
+
+print(ans)
--- abc_old/abc390/d/main.py
+++ abc_old/abc390/d/main.py
@@ -1,2 +1,51 @@
+# 最大ケースで6s程度。悲しい
+
+def divide_group_nocopy(N: int):
+    """要素ごとにコピーをしない高速化バージョン。
+       N=12で0.7s, N=13で5.6s程度。
+
+    Args:
+        N (int): 要素の数
+
+    """
+
+    assert N >= 0
+
+    dfs = [(0, [], -1)]
+    while len(dfs) > 0:
+        finished_count, groups, added_i = dfs.pop()
+
+        if finished_count < 0:
+            groups[added_i].remove(-finished_count - 1)
+            if len(groups[added_i]) == 0:
+                groups.pop()
+
+            continue
+
+        if added_i >= 0:
+            if added_i == len(groups):
+                groups.append({finished_count - 1})
+            else:
+                groups[added_i].add(finished_count - 1)
+
+        if finished_count >= N:
+            yield groups
+            continue
+
+        for i in range(len(groups) + 1):
+            dfs.append((-finished_count - 1, groups, i))
+            dfs.append((finished_count + 1, groups, i))
+
+# -------------------
+
 N = int(input())
 A = [int(x) for x in input().split()]
+
+ans = set()
+for groups in divide_group_nocopy(N):
+    stones = 0
+    for group in groups:
+        stones ^= sum(A[i] for i in group)
+    ans.add(stones)
+
+print(len(ans))
