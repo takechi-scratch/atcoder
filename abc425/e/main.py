4198d321215c8fff1c78792a732aab59a188230d
takechi-scratch <takechi.scratch@gmail.com>
2025-10-04 17:14:22 +0900

    upsolve abc425e

--- abc425/e/main.py
+++ abc425/e/main.py
@@ -1,68 +1,38 @@
-import subprocess
-from collections import Counter
-from functools import lru_cache
-from math import isqrt
-import sys
-
-sys.setrecursionlimit(10**7)
-
-
-def prime_factorization(x: int) -> list[int]:
-    """素因数分解を行う関数
-
-    Args:
-        x (int): 素因数分解したい整数
-
-    Returns:
-        list[int]: 素因数分解した結果のリスト
-    """
-
-    out = subprocess.run(["factor", str(x)], stdout=subprocess.PIPE, check=True).stdout
-    return [int(x) for x in out.decode().strip().split(" ")[1:]]
-
-
-prime_max = 5000
-prime_kouho = set(range(2, prime_max))
-for i in range(2, isqrt(prime_max) + 1):
-    if i not in prime_kouho:
-        continue
-
-    j = 2
-    while i * j <= prime_max:
-        prime_kouho.discard(i * j)
-        j += 1
-
-primes = list(sorted(prime_kouho))
-
-
-@lru_cache(maxsize=None)
-def factor_counter(n: int) -> Counter:
-    if n <= 1:
-        return {}
-
-    ans = factor_counter(n - 1)
-    for x in prime_factorization(n):
-        ans[x] += 1
-
-    return ans
-
-
-factor_counter(5000)
-
 T, M = [int(x) for x in input().split()]
 
-
+testcases = []
 for _ in range(T):
     N = int(input())
     C = [int(x) for x in input().split()]
-    ans = factor_counter(sum(C)).copy()
+    testcases.append([N, C])
+
+max_C = max(sum(C) for _, C in testcases)
+# print(max_C)
+
+# 組み合わせの計算はこの漸化式でできる！！（数学でやったやつ）
+# 非素数modでも、nが小さければ使える。
+comb = [[0] * (max_C + 1) for _ in range(max_C + 1)]
+for n in range(1, max_C + 1):
+    for k in range(max_C + 1):
+        if n < k:
+            continue
+
+        if n == 1:
+            comb[n][k] = 1
+        elif k == 0:
+            comb[n][k] = 1
+        else:
+            comb[n][k] = (comb[n - 1][k] + comb[n - 1][k - 1]) % M
+
+# print(comb[5][2])
+# assert comb[5][2] == 10
+
+for N, C in testcases:
+    sum_C = sum(C)
+    ans = 1
     for x in C:
-        for key, value in factor_counter(x).items():
-            if key > 1:
-                ans[key] -= value
+        ans *= comb[sum_C][x]
+        ans %= M
+        sum_C -= x
 
-    last_ans = 1
-    for key, value in ans.items():
-        last_ans *= key**value
-
-    print(last_ans % M)
+    print(ans)
