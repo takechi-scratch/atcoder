b60750cf7386d1e6af78b9a184e47900ae48d2d3
takechi-scratch <takechi.scratch@gmail.com>
2025-10-05 21:51:32 +0900

    upsolve abc426

--- abc426/e/main.py
+++ abc426/e/main.py
@@ -1,99 +1,73 @@
-from math import gcd, sqrt
+# 解説AC。（まだできていない）
+# 困難は分割せよ→同時に2点動いているときと、1点の移動が終わり残りの1点が動いているとき
+# 同時に動くのはしんどいので、1点を固定し「点と直線の距離」を用いる
+
+from math import sqrt
 
 
-def dist(x1, y1, x2, y2):
+def get_dist(x1, y1, x2=0, y2=0):
     return sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
 
-def cross_x(line1: "Lines", line2: "Lines"):
-    if line1.a * line2.b - line2.a * line1.b == 0:
-        return 10**18
+def nearest_point_line(base_sx: int, base_sy: int, dx: int, dy: int, start: int, goal: int):
+    dist = goal - start
+    # WAポイント！ゼロ割りに気を付ける。
+    # ベクトルの正規化で長さが0の時に注意。
+    if dist <= 0 or get_dist(dx, dy) <= 0:
+        return get_dist(base_sx, base_sy)
 
-    return (line1.b * line2.c - line2.b * line1.c) / (line1.a * line2.b - line2.a * line1.b)
+    dx, dy = dx / get_dist(dx, dy), dy / get_dist(dx, dy)
 
+    # 三分探索
+    # 極値を1つだけ持つ関数の最小値を求める！！
+    while goal - start > 10**-12:
+        mid1 = start + (goal - start) / 3
 
-class Lines:
-    def __init__(self, a: int, b: int, c: int):
-        abc_gcd = gcd(abs(a), abs(b), abs(c))
-        if a < 0:
-            a = -a
-            b = -b
-            c = -c
+        check_x = base_sx + dx * (mid1 / dist) * dist
+        check_y = base_sy + dy * (mid1 / dist) * dist
+        dist1 = get_dist(check_x, check_y)
 
-        if a == 0 and b < 0:
-            b = -b
-            c = -c
+        mid2 = start + (goal - start) / 3 * 2
+        check_x = base_sx + dx * (mid2 / dist) * dist
+        check_y = base_sy + dy * (mid2 / dist) * dist
+        dist2 = get_dist(check_x, check_y)
 
-        self.a = a // abc_gcd
-        self.b = b // abc_gcd
-        self.c = c // abc_gcd
+        if dist1 == dist2:
+            break
+        if dist1 < dist2:
+            goal = mid2
+        else:
+            start = mid1
 
-    def through_point(self, x: int, y: int):
-        return self.a * x + self.b * y + self.c == 0
-
-    def get_y(self, x: int):
-        return (self.c - self.a * x) / self.b
-
-    def __hash__(self):
-        return hash((self.a, self.b, self.c))
-
-    def __repr__(self):
-        return f"Lines({self.a}, {self.b}, {self.c})"
+    mid = (start + goal) / 2
+    check_x = base_sx + dx * (mid / dist) * dist
+    check_y = base_sy + dy * (mid / dist) * dist
+    return get_dist(check_x, check_y)
 
 
 def solve(TSx: int, TSy: int, TGx: int, TGy: int, ASx: int, ASy: int, AGx: int, AGy: int):
-    Tline = Lines(TGy - TSy, -TGx + TSx, TGx * TSy - TSx * TGy)
-    T_dist = dist(TSx, TSy, TGx, TGy)
-    Aline = Lines(AGy - ASy, -AGx + ASx, AGx * ASy - ASx * AGy)
-    A_dist = dist(ASx, ASy, AGx, AGy)
+    T_dist = get_dist(TSx, TSy, TGx, TGy)
+    A_dist = get_dist(ASx, ASy, AGx, AGy)
     ans = 10**18
 
-    # 交点
-    cx = cross_x(Tline, Aline)
-    if min(TSx, TGx) <= cx <= max(TSx, TGx):
-        cy = Tline.get_y(cx)
-        cosx = sqrt(1 / (1 + ((AGy - ASy) / (AGx - ASx)) ** 2))
-        if ASx > AGx:
-            cosx *= -1
+    if T_dist > A_dist:
+        TSx, TSy, TGx, TGy, ASx, ASy, AGx, AGy = ASx, ASy, AGx, AGy, TSx, TSy, TGx, TGy
+        T_dist, A_dist = A_dist, T_dist
 
-        cross_Ax = ASx + min(dist(TSx, TSy, cx, cy), A_dist) * cosx
-        ans = min(ans, dist(cx, cy, cross_Ax, Aline.get_y(cross_Ax)))
+    Tdx, Tdy = TGx - TSx, TGy - TSy
+    Adx, Ady = AGx - ASx, AGy - ASy
 
-        cy = Aline.get_y(cx)
-        cosx = sqrt(1 / (1 + ((TGy - TSy) / (TGx - TSx)) ** 2))
-        if TSx > TGx:
-            cosx *= -1
+    # Tの長さ分にAのdxdyを縮小させる！
+    Adx, Ady = Adx * (T_dist / A_dist), Ady * (T_dist / A_dist)
 
-        cross_Ax = TSx + min(dist(ASx, ASy, cx, cy), T_dist) * cosx
-        ans = min(ans, dist(cx, cy, cross_Ax, Tline.get_y(cross_Ax)))
+    # ベクトルを引き算して、Tを固定する
+    move_dx = Adx - Tdx
+    move_dy = Ady - Tdy
 
-    # 始点・終点
-    ans = min(ans, dist(TSx, TSy, ASx, ASy))
-    # ans = min(ans, diff(TSx, TSy, AGx, AGy))
-    # ans = min(ans, diff(TGx, TGy, ASx, ASy))
-    ans = min(ans, dist(TGx, TGy, AGx, AGy))
-
-    if T_dist <= A_dist:
-        x0, y0 = TGx, TGy
-        goal_base_x = ASx + (T_dist / A_dist) * (AGx - ASx)
-        base_line = Aline
-        base_Sx, base_Gx = ASx, AGx
-    else:
-        x0, y0 = AGx, AGy
-        goal_base_x = TSx + (A_dist / T_dist) * (TGx - TSx)
-        base_line = Tline
-        base_Sx, base_Gx = TSx, TGx
-
-    vert_line = Lines(base_line.b, -base_line.a, base_line.a * y0 - base_line.b * x0)
-
-    cx = cross_x(base_line, vert_line)
-    if min(base_Sx, goal_base_x) <= cx <= max(base_Sx, goal_base_x):
-        goal_base_y = base_line.get_y(goal_base_x)
-        ans = min(ans, dist(x0, y0, goal_base_x, goal_base_y))
-    else:
-        # 点と直線の距離
-        pl_dist = abs(base_line.a * x0 + base_line.b * y0 + base_line.c) / sqrt(base_line.a**2 + base_line.b**2)
-        ans = min(ans, pl_dist)
+    # Tをpointとして固定、Aをbaseに
+    ans = min(ans, nearest_point_line(ASx - TSx, ASy - TSy, move_dx, move_dy, 0, get_dist(move_dx, move_dy)))
+    # Tが終点にいるとき、Aをbaseに
+    ans = min(ans, nearest_point_line(ASx - TGx, ASy - TGy, Adx, Ady, T_dist, A_dist))
 
     return ans
 
@@ -104,4 +78,4 @@ for _ in range(T):
     ASx, ASy, AGx, AGy = [int(x) for x in input().split()]
 
     ans = solve(TSx, TSy, TGx, TGy, ASx, ASy, AGx, AGy)
-    print(ans)
+    print(f"{ans:.12f}")
