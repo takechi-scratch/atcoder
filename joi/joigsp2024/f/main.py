65ddf5b1db91c732c9202ffc29f991c3349c0d6c
takechi-scratch <takechi.scratch@gmail.com>
2025-11-15 20:58:18 +0900

    stash joi_practice12

--- joi/joigsp2024/f/main.py
+++ joi/joigsp2024/f/main.py
@@ -1,2 +1,20 @@
-# N = int(input())
-# A = [int(x) for x in input().split()]
+# まだできてない
+
+N, M = [int(x) for x in input().split()]
+chocolates = [[int(x) for x in input().split()] for _ in range(N)]
+
+chocolates.sort(key=lambda x: (x[0] - x[1], x[0]))
+
+choco_res = []
+for start in range(M):
+    now = start
+    tickets = start
+    for a, b in chocolates:
+        if tickets < a:
+            continue
+
+        exchange_count = (tickets - a) // (a - b) + 1
+        now += b * exchange_count
+        tickets += (b - a) * exchange_count
+
+    choco_res.append(now)
