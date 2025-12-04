47aa0837d5e0fdc90c7d931167d90fde1678b005
takechi-scratch <takechi.scratch@gmail.com>
2025-12-04 22:49:18 +0900

    add joi_practice15

--- joi/joi2022yo2/d/main.py
+++ joi/joi2022yo2/d/main.py
@@ -1,26 +1,33 @@
-# 途中
+# 解説AC。DPの持つ情報は「最後にどこの飴を食べたか」（N^2）
+# ナップサックDPにとらわれて「～番目までに」とすると解けない！！！！
 
 N, K = [int(x) for x in input().split()]
 A = [int(x) for x in input().split()]
 
-dp = [[[-(10**18)] * 3 for _ in range(K + 1)] for _ in range(N + 1)]
-dp[-1] = [[0] * 3 for _ in range(K + 1)]
-
-# i番目までの飴で、直近のj個の中でk個までを食べたときのおいしさの最大値
+dp = [[None for _ in range(N)] for _ in range(N)]
 for i in range(N):
-    for j in range(1, K + 1):
-        if j == 1:
-            dp[i][j][0] = 0
-            dp[i][j][1] = A[i]
+    for j in range(N):
+        # i==jは考慮しなくてよい（バグの原因）
+        if i - j <= 0:
+            dp[i][j] = -(10**18)
+        else:
+            dp[i][j] = A[i] + A[j]
+
+max_dp = [[-(10**18) for _ in range(N)] for _ in range(N)]
+ans = 0
+
+# 1個前がi番目、2個前がj番目
+for i in range(N):
+    for j in range(N):
+        if dp[i][j] < 0:
             continue
 
-        dp[i][j][0] = dp[i - 1][j - 1][0]
-        if
-            dp[i][j][0] = max(dp[i - 1][j - 1])
-        dp[i][j][1] = max(dp[i - 1][j - 1][1], dp[i - 1][j - 1][0] + A[i])
-        if j == 2:
-            dp[i][j][2] = dp[i - 1][j - 1][1] + A[i]
-        else:
-            dp[i][j][2] = max(dp[i - 1][j - 1][2], dp[i - 1][j - 1][1] + A[i])
+        # max_dp[j][min(j - 1, i - K)]が、1個より前の条件を満たす最大スコア
+        dp[i][j] = max(dp[i][j], max_dp[j][min(j - 1, i - K)] + A[i])
 
-print(max(dp[N - 1][K]))
+        max_dp[i][j] = max(max_dp[i][j], dp[i][j])
+        if j - 1 >= 0:
+            max_dp[i][j] = max(max_dp[i][j], max_dp[i][j - 1])
+        ans = max(ans, dp[i][j])
+
+print(ans)
