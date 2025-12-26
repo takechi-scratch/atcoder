d3a177ef29a6c3e78828b1293fb0ac07ea5823e2
takechi-scratch <takechi.scratch@gmail.com>
2025-12-26 18:46:17 +0900

    add acl-codon

--- others/language-test-202505/practice2a/main.py
+++ others/language-test-202505/practice2a/main.py
@@ -1,2 +1,11 @@
-# N = int(input())
-# A = [int(x) for x in input().split()]
+from atcoder.dsu import DSU
+
+N, Q = [int(x) for x in input().split()]
+
+uf = DSU(N)
+for _ in range(Q):
+    t, u, v = [int(x) for x in input().split()]
+    if t == 0:
+        uf.merge(u, v)
+    else:
+        print(int(uf.same(u, v)))
