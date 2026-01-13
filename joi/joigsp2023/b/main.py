6e0252c2c0c959716138bed202afa572efad981e
takechi-scratch <takechi.scratch@gmail.com>
2026-01-13 22:50:05 +0900

    add joi_sf_practice2

--- joi/joigsp2023/b/main.py
+++ joi/joigsp2023/b/main.py
@@ -1,2 +1,45 @@
-# N = int(input())
-# A = [int(x) for x in input().split()]
+# 結局は「後ろから見る」のが強い（BFSにもちこむ）
+
+from collections import deque
+
+N = int(input())
+teleporters = []
+for _ in range(N - 1):
+    a = int(input())
+    teleporters.append([[int(x) - 1 for x in input().split()] for _ in range(a)])
+teleporters.append([])
+
+sides = [[] for _ in range(N)]
+for i, x in enumerate(teleporters):
+    for j, y in enumerate(x):
+        p, q = y
+        sides[p].append((i, j, 0))
+        sides[q].append((i, j, 1))
+
+score = [10**18] * N
+score[-1] = 0
+
+bfs = deque([(N - 1, 0)])
+while len(bfs) > 0:
+    now, now_score = bfs.popleft()
+    if now_score > score[now]:
+        continue
+
+    for next_i, next_j, next_k in sides[now]:
+        next_other_score = score[teleporters[next_i][next_j][1 - next_k]]
+
+        if next_other_score == 10**18:
+            continue
+
+        next_score = max(next_other_score, now_score) + 1
+
+        if next_score >= score[next_i]:
+            continue
+
+        score[next_i] = next_score
+        bfs.append((next_i, next_score))
+
+if score[0] == 10**18:
+    print(-1)
+else:
+    print(score[0])
--- joi/joigsp2023/e/main.py
+++ joi/joigsp2023/e/main.py
@@ -1,2 +1,42 @@
-# N = int(input())
-# A = [int(x) for x in input().split()]
+import heapq
+
+N, K = [int(x) for x in input().split()]
+phones = [[int(x) for x in input().split()] for _ in range(N)]
+left_phone_days = [x[2] for x in phones]
+
+ans = 0
+events = []
+for i, phone in enumerate(phones):
+    heapq.heappush(events, (phone[0] - 1, 1, i))
+    heapq.heappush(events, (phone[1], 2, i))
+heapq.heappush(events, (K, -1, -1))
+
+usable = []
+ans = 0
+before = 0
+while len(events) > 0:
+    day, event_type, event_i = heapq.heappop(events)
+    left_days = day - before
+    while left_days > 0 and len(usable) > 0:
+        limit, i = heapq.heappop(usable)
+        if day > limit:
+            continue
+
+        if left_days <= left_phone_days[i]:
+            left_phone_days[i] -= left_days
+            left_days = 0
+            if left_phone_days[i] > 0:
+                heapq.heappush(usable, (limit, i))
+            break
+        else:
+            left_days -= left_phone_days[i]
+            left_phone_days[i] = 0
+
+    ans += (day - before) - left_days
+    before = day
+    if event_type == 1:
+        heapq.heappush(usable, (phones[event_i][1], event_i))
+    # else:
+    #     usable.discard((phones[event_i][1], event_i))
+
+print(ans)
