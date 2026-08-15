401ef51f825a33dda39ba659ec9ba041f75d98d6
takechi-scratch <takechi.scratch@gmail.com>
2026-08-15 20:56:35 +0900

    add abc_practice

--- abc470/c/main.py
+++ abc470/c/main.py
@@ -1,8 +1,24 @@
+# Codon動作確認済み
+
 N, Q = [int(x) for x in input().split()]
+A = [0] * N
+nonzero = set()
+ans = 0
 
 for _ in range(Q):
     query = [int(x) for x in input().split()]
     if query[0] == 1:
         x = query[1] - 1
+        ans ^= A[x]
+        A[x] += 1
+        ans ^= A[x]
+        nonzero.add(x)
     else:
-        pass
+        for x in nonzero.copy():
+            ans ^= A[x]
+            A[x] -= 1
+            ans ^= A[x]
+            if A[x] == 0:
+                nonzero.remove(x)
+
+    print(ans)
